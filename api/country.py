45acfd1c6a64848d064d7e538d7f263da336c3ac
unknown <lailaibraheem@hotmail.com>
2022-07-04 22:14:57 +0300

    feat

--- api/country.py
+++ api/country.py
@@ -1,26 +1,32 @@
 from http.server import BaseHTTPRequestHandler
-import requests
 from urllib import parse
+import requests
 
-class Capital(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         url_components = parse.urlsplit(s)
+        print(url_components)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
-        Capital = ""
-        if 'country' in dic:
-            word = dic['country']
+        print(dic)
+        definitions = []
+        if 'word' in dic:
+            word = dic['word']
             url = 'https://restcountries.com/v3.1/name/'
             r = requests.get(url + word)
             data = r.json()
-            for capital in data:
-                Capital = capital['capital'][0]
+            print(data)
+            for word_data in data:
 
-            message = str(Capital)
+                definition = f"The capital of {word} is {word_data['capital'][0]}"
+                definitions.append(definition)
+
+            message = str(definitions)
 
         else:
-            message = "Please provide us a correct country Name"
+            message = "Please provide me with a word"
+
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
